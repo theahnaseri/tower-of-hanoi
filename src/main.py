2775049115503b3259292d30a2f75473fd1a4ad5
Maryam Fakhraei <M.Fakhraeisari@gmail.com>
2023-03-13 13:50:14 +0330

    Add List As Towers

--- src/main.py
+++ src/main.py
@@ -1,20 +1,18 @@
-# Tower Of Hanoi main algorithm function for n disks
 def TowerOfHanoi(n, from_tower, to_tower, aux_tower):
-	# Base case (continue until move all disks)
-	if n == 0:
-		return
-	
-	# Move subproblem of n-1 disks from source tower to helper tower
-	TowerOfHanoi(n-1, from_tower, aux_tower, to_tower)
+    if n == 0:
+        return
+    TowerOfHanoi(n-1, from_tower, aux_tower, to_tower)
 
-	# Print status
-	print("Move disk %i from %s tower to %s tower" %(n, from_tower, to_tower))
+    to_tower.append(from_tower[-1])
+    del from_tower[-1]
+    print(source, helper, destin)
+    TowerOfHanoi(n-1, aux_tower, to_tower, from_tower)
 
-	# Move subproblem of n-1 disks from helper tower to destination tower
-	TowerOfHanoi(n-1, aux_tower, to_tower, from_tower)
 
-# Get disk count from user
 disk_count = int(input("disk count : "))
-
-# Call main function
-TowerOfHanoi(disk_count, '|Source|', '|Destin|', '|Helper|')
+source = list(range(1, disk_count+1))
+source.reverse()
+destin = []
+helper = []
+print(source, helper, destin)
+TowerOfHanoi(disk_count, source, destin, helper)
